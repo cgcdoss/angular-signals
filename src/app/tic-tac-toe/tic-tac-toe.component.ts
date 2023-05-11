8eb9a7273a0e1c83fba4da9d5051c309b14e1362
cgcdoss <gabi04061997@gmail.com>
2023-05-10 22:50:11 -0300

    refactor: melhorando forma de detectar empate

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -62,11 +62,7 @@ export class TicTacToeComponent {
     }
 
     // Deu velha
-    if (
-      game[0][0] && game[0][1] && game[0][1] &&
-      game[1][0] && game[1][2] && game[1][2] &&
-      game[2][0] && game[2][1] && game[2][2]
-    ) {
+    if (game.flat().filter(r => !!r).length === 9) {
       return 'Deu velha!';
     }
 
